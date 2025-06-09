61f045f92471d92d322650a2f7d807f71456f575
alammd0 <sonualam96314@gmail.com>
2025-06-09 12:49:54 +0530

    add

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: ["**/generated/**"], // ⬅️ Add this
+    ignores: ["**/generated/**"],
     rules: {
       "@typescript-eslint/no-this-alias": "off",
     },
